5cb98c4ef42f62b3ea458d53af8e9221631add18
aravindsekhar76 <115882981+aravindsekhar76@users.noreply.github.com>
2023-01-15 09:56:28 +0530

    Update singlyll.c

--- singlyll.c
+++ singlyll.c
@@ -1,269 +1,213 @@
-#include <stdio.h>
-#include <malloc.h>
-#include <stdlib.h>
-struct node
-{
-	int data;
-	struct node *next;
-};
-struct node *start = NULL;
-struct node *create(struct node *);
-struct node *display(struct node *);
-struct node *insert_beg(struct node *);
-void insert_pos();
-struct node *insert_end();
-struct node *delete_beg(struct node *);
-struct node *delete_end(struct node *);
-void delete_pos();
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void insbeg();
+void inspos();
+void insend();
+void delbeg();
+void delend();
+void delpos();
+void search();
 int count();
-int main()
-{
-	int option;
-	do
-	{
-		printf("\n\nMENU");
-		printf("\n\n1. CREATE LINKED LIST");
-		printf("\n\n2. DISPLAY LINKED LIST");
-		printf("\n\n3. INSERT AT BEGINNING");
-		printf("\n\n4. INSERT AT END");
-		printf("\n\n5. INSERT AT POSITION");
-		printf("\n\n6. DELETE FROM BEGINNING");
-		printf("\n\n7. DELETE FROM END");
-		printf("\n\n8. DELETE FROM A POSITION");
-		printf("\n\n9. EXIT");
-		printf("\n\nEnter your option : ");
-		scanf("%d", &option);
-		switch (option)
-		{
-		case 1:
-			start = create(start);
-			printf("LINKED LIST CREATED");
-			break;
-		case 2:
-			start = display(start);
-			break;
-		case 3:
-			start = insert_beg(start);
-			break;
-		case 4:
-			start = insert_end();
-			break;
-		case 5:
-			insert_pos();
-			break;
-		case 6:
-			start = delete_beg(start);
-			break;
-		case 7:
-			start = delete_end(start);
-			break;
-		case 8:
-			delete_pos();
-			break;
-		}
-	} while (option < 9);
-	return 0;
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *newnode, *head=NULL;
+
+int count(){
+    struct node *temp;
+    int c = 0;
+    temp = head;
+    while(temp != NULL){
+        temp = temp->next;
+        c++;
+    }
+    return(c);
+}
+void insbeg() {
+    int val;
+    printf("Enter the value : ");
+    scanf("%d", &val);
+    newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = val;
+    newnode->next = head;
+    head = newnode;
 }
 
-struct node *create(struct node *start)
-{
-	struct node *ptr, *newnode;
-	int num;
-	printf("Enter -1 to end\n");
-	printf("\nEnter the data : ");
-	scanf("%d", &num);
-	while (num != -1)
-	{
-		newnode = (struct node *)malloc(sizeof(struct node));
-		newnode->data = num;
-		newnode->next = NULL;
-		if (start == NULL)
-		{
-			start = newnode;
-		}
-		else
-		{
-			ptr = start;
-			while (ptr->next != NULL)
-			{
-				ptr = ptr->next;
-			}
-			ptr->next = newnode;
-		}
-		printf("\nEnter the data : ");
-		scanf("%d", &num);
-	}
-	return start;
+void traversal(){
+    struct node *temp;
+    // printf("count = %d",count());
+    if(head == NULL){
+        printf("Linked List empty");
+    }
+    else{
+        temp = head;
+        while(temp!= NULL){
+            printf("%d\n",temp->data);
+            temp = temp->next;
+        }
+    }
+}
+void insend(){
+    struct node *temp;
+    int val;
+    if(head == NULL){
+        insbeg();
+    }
+    else{
+        printf("Enter the value : ");
+        scanf("%d", &val);
+        newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->data = val;
+        newnode->next = NULL;
+        temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newnode;
+    }
 }
 
-struct node *display(struct node *start)
-{
-	struct node *ptr;
 
-	if (start == NULL)
-	{
-		printf("\n\nLINKED LIST IS EMPTY");
-	}
-	else
-	{
-		ptr = start;
-		printf("\n\n\t*****LINKED LIST*****\n\n");
-		while (ptr != NULL)
-		{
-			printf("%d\t", ptr->data);
-			ptr = ptr->next;
-		}
-	}
-	return start;
+
+void inspos(){
+    int pos, val;
+    struct node *temp;
+    printf("Enter position : ");
+    scanf("%d", &pos);
+    if(pos == 1){
+        insbeg();
+    }
+    else if(pos > 1 && pos <= count()){
+        printf("Enter the value:");
+        scanf("%d", &val);
+        newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->data = val;
+        temp = head;
+        int i = 1;
+        while(i < pos-1){
+            temp = temp->next;
+            i++;
+        }
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+    else if(pos == (count()+1)){
+        insend();
+    }
+    else{
+        printf("Invalid position");
+    }
+    
+}
+void delbeg(){
+    if(head == NULL){
+        printf("Linked List empty");
+    }
+    else{
+        struct node *temp;
+        temp = head;
+        head = head->next;
+        printf("%d is deleted", temp->data);
+    }
 }
 
-struct node *insert_beg(struct node *start)
-{
-	int num;
-	struct node *newnode;
-	printf("Enter data to insert at begining : ");
-	scanf("%d", &num);
-	newnode = (struct node *)malloc(sizeof(struct node));
-	newnode->data = num;
-	newnode->next = start;
-	start = newnode;
-	return start;
+void delend(){
+    if(head == NULL){
+        printf("Linked List empty");
+    }
+    else if(head->next == NULL){
+        printf("%d is deleted", head->data);
+        head = NULL;
+    }
+    else{
+        struct node *temp;
+        temp = head;
+        while(temp->next->next != NULL){
+            temp = temp->next;
+        }
+        printf("%d is deleted", temp->next->data);
+        temp->next = NULL;
+    }
+}
+void delpos(){
+    int pos,i;
+    struct node *temp;
+    printf("Enter the position to delete : ");
+    scanf("%d", &pos);
+    if(pos == 1){
+        delbeg();
+    }
+    else if(pos > 1 && pos < count()){
+        temp = head;
+        i = 1;
+        while(i < pos-1){
+            temp = temp->next;
+            i++;
+        }
+        printf("%d is deleted", temp->next->data);
+        temp->next = temp->next->next;
+    }
+    else if(pos == count()){
+        delend();
+    }
+    else{
+        printf("Invalid position");
+    }
 }
 
-struct node *insert_end()
-{
-	int num;
-	struct node *newnode, *ptr;
-	printf("Enter data to insert at end : ");
-	scanf("%d", &num);
-	newnode = (struct node *)malloc(sizeof(struct node));
-	newnode->data = num;
-	newnode->next = NULL;
-	ptr = start;
-	while (ptr->next != NULL)
-	{
-		ptr = ptr->next;
-	}
-	ptr->next = newnode;
-	return start;
+void search(){
+    int e, f=0;
+    struct node *temp;
+    printf("Enter the element to be searched : ");
+    scanf("%d", &e);
+    temp = head;
+    while(temp != 0){
+        if(temp->data == e){
+            printf("\nElement found");
+            f = 1;
+            break;
+        }
+        temp = temp->next;
+    }
+    if(f == 0){
+        printf("Element not found");
+    }
 }
 
-void insert_pos()
-{
-	int cnt, pos, i = 1, data;
-	struct node *ptr, *preptr, *newnode;
-	ptr = start;
-	cnt = count();
-	while (ptr != NULL)
-	{
-		cnt++;
-		ptr = ptr->next;
-	}
-	printf("Total number of elements in linked list : %d. You can insert new elements based on this count", cnt);
-	printf("\nEnter the position : ");
-	scanf("%d", &pos);
-	ptr = start;
-	if ((cnt >= pos || pos == cnt + 1) && pos != 0)
-	{
+void main(){
+    int choice;
+    while(1){
+        printf("\n--------------------------------------------\n");
+        printf("\n1.Insertion beg\n2.Insertion postion\n3.Insertion end\n4.Traversal\n5.Deletion beg\n6.Deletion end\n7.Deletion pos\n8.Search\n9.Exit\n");
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1: insbeg();
+                    break;
+            case 2: inspos();
+                    break;
+            case 3: insend();
+                    break;
+            case 4: traversal();
+                    break;
+            case 5: delbeg();
+                    break;
+            case 6: delend();
+                    break;
+            case 7: delpos();
+                    break;
+            case 8: search();
+                    break;
+            case 9: exit(0);
+                    break;
+            default: 
+                    printf("Invalid Choice");
+        }
+    }
 
-		newnode = (struct node *)malloc(sizeof(struct node));
-		printf("Enter data : ");
-		scanf("%d", &data);
-		newnode->data = data;
-		newnode->next = NULL;
-
-		while (i < pos)
-		{
-			preptr = ptr;
-			ptr = ptr->next;
-			i++;
-		}
-
-		if (start == ptr)
-		{
-			newnode->next = ptr;
-			start = newnode;
-		}
-		else
-		{
-			preptr->next = newnode;
-			newnode->next = ptr;
-		}
-	}
-
-	else
-	{
-		printf("invalid");
-	}
-}
-
-struct node *delete_beg(struct node *start)
-{
-	struct node *ptr;
-	ptr = start;
-	start = start->next;
-	free(ptr);
-	return start;
-}
-
-struct node *delete_end(struct node *start)
-{
-	struct node *ptr, *preptr;
-	ptr = start;
-	while (ptr->next != NULL)
-	{
-		preptr = ptr;
-		ptr = ptr->next;
-	}
-
-	preptr->next = NULL;
-	free(ptr);
-	return start;
-}
-void delete_pos()
-{
-
-	struct node *ptr, *preptr;
-	int pos, i = 1, cnt;
-	cnt = count();
-	printf("\n\nTotal number of elements in linked list is %d. You can delete element based on this count", cnt);
-	printf("\n\nEnter the position : ");
-	scanf("%d", &pos);
-	ptr = start;
-
-	if ((cnt >= pos && pos != 0))
-	{
-		while (i<pos)
-		{
-			preptr = ptr;
-			ptr = ptr->next;
-			i++;
-		}
-
-		if(start == ptr){
-			start = ptr -> next;
-			free(ptr);
-		}
-		else{
-			preptr -> next = ptr -> next;
-		}
-	}
-
-	else{
-		printf("Not Possible");
-	}
-}
-
-int count()
-{
-	int count = 0;
-	struct node *ptr;
-	ptr = start;
-	while (ptr != NULL)
-	{
-		count++;
-		ptr = ptr->next;
-	}
-
-	return count;
 }
